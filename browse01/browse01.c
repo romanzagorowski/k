bc66bf0f0e4041feb95495765b3b0d5df75dc2e4
Roman Zagorowski <roman.zagorowski@hotmail.com>
2025-04-02 20:17:01 +0200

    project browse01 adopted to ubuntu 22

--- browse01/browse01.c
+++ browse01/browse01.c
@@ -2,13 +2,13 @@
 
 static int browse01_init(void)
 {
-    pr_debug("static int browse01_init(void)\n");
+    pr_info("static int browse01_init(void)\n");
     return 0;
 }
 
 static void browse01_exit(void)
 {
-    pr_debug("static void browse01_exit(void)\n");
+    pr_info("static void browse01_exit(void)\n");
 }
 
 module_init(browse01_init);

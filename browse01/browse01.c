8151ef6a41d953bdba7e3781e5b5b71cdcdea30e
Roman Zagorowski <roman.zagorowski@hotmail.com>
2025-04-27 07:45:49 +0200

    browse01: pr_debug macro tries

--- browse01/browse01.c
+++ browse01/browse01.c
@@ -1,14 +1,18 @@
+//#define DEBUG
+
 #include <linux/module.h>
 
 static int browse01_init(void)
 {
-    pr_info("static int browse01_init(void)\n");
+    pr_info("browse01_init: pr_info\n");
+    pr_debug("browse01_init: pr_debug\n");
     return 0;
 }
 
 static void browse01_exit(void)
 {
-    pr_info("static void browse01_exit(void)\n");
+    pr_info("browse01_exit: pr_info\n");
+    pr_debug("browse01_exit: pr_debug\n");
 }
 
 module_init(browse01_init);
